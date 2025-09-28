c46392ceb1951461723aaf55c0babe49cac27327
Eduardo Alves <eduardo2005alve@gmail.com>
2025-09-28 20:59:02 -0300

    Criação da entidade usuario criada completamente

--- src/Usuario/entities/usuario.entity.ts
+++ src/Usuario/entities/usuario.entity.ts
@@ -1,50 +1,53 @@
-import { Type } from 'class-transformer';
-import {
-  IsDate,
-  IsNotEmpty,
-  Matches,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
+import { IsNotEmpty, Matches, MaxLength, MinLength } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('tb_usuarios')
 export class Usuarios {
+  // ID do usuário - autoincrement
   @PrimaryGeneratedColumn()
   id: number;
 
-  @IsNotEmpty()
+  // Nome do usuário
+  @IsNotEmpty({ message: 'O nome não pode estar vazio' })
+  @MinLength(3, { message: 'O nome não pode ter menos de 3 caracteres' })
+  @MaxLength(255, { message: 'O nome não pode passar de 255 caracteres' })
   @Column({ length: 255, nullable: false })
   nome: string;
 
-  @IsNotEmpty()
-  @Column({ length: 255, nullable: false })
+  // Email do usuário
+  @IsNotEmpty({ message: 'O email não pode estar vazio' })
+  @MinLength(10, { message: 'O email não pode ter menos de 10 caracteres' })
+  @Matches(/^[^\s@]+/, {
+    message: 'O email deve começar com caracteres válidos',
+  })
+  @Matches(/@/, { message: 'O email deve conter o @' })
+  @Matches(/\.[^\s@]+$/, {
+    message: 'O email deve terminar com um domínio válido',
+  })
+  @Column({ length: 255, nullable: false, unique: true })
   email: string;
 
+  // Senha do usuário
   @IsNotEmpty({ message: 'A senha não pode estar vazia' })
   @MinLength(8, { message: 'A senha deve ter no mínimo 8 caracteres' })
   @MaxLength(32, { message: 'A senha deve ter no máximo 32 caracteres' })
   @Matches(/(?=.*[A-Z])/, {
     message: 'A senha deve conter pelo menos uma letra maiúscula',
-    // message: define a mensagem de erro personalizada que será retornada se a validação falhar.
   })
   @Matches(/(?=.*[a-z])/, {
     message: 'A senha deve conter pelo menos uma letra minúscula',
-    // message: define a mensagem de erro personalizada que será retornada se a validação falhar.
   })
   @Matches(/(?=.*[0-9])/, {
     message: 'A senha deve conter pelo menos um número',
-    // message: define a mensagem de erro personalizada que será retornada se a validação falhar.
   })
   @Matches(/(?=.*[!@#$%^&*])/, {
     message: 'A senha deve conter pelo menos um caractere especial (!@#$%^&*)',
-    // message: define a mensagem de erro personalizada que será retornada se a validação falhar.
   })
   @Column({ length: 255, nullable: false })
   senha: string;
 
+  // Data de nascimento
   @IsNotEmpty({ message: 'A data de nascimento é obrigatória' })
-  @Type(() => Date) // transforma string em Date automaticamente
-  @IsDate({ message: 'Data inválida, use o formato YYYY-MM-DD' }) // Verifica se o valor é realmente uma data (objeto Date válido do JavaScript).
+  @Column()
   dataNascimento: Date;
 }
